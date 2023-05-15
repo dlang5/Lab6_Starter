593fff7fa941375743ba39395b1c7a659ffef5c5
dlang5 <dlang@ucsd.edu>
2023-05-14 23:49:57 -0700

    Update main.js

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -21,9 +21,17 @@ function init() {
  * @returns {Array<Object>} An array of recipes found in localStorage
  */
 function getRecipesFromStorage() {
-  // A9. TODO - Complete the functionality as described in this function
+// A9. TODO - Complete the functionality as described in this function
   //           header. It is possible in only a single line, but should
   //           be no more than a few lines.
+  const recipe = localStorage.getItem('recipes');
+  if (!recipe){
+    return [];
+  } 
+   else{
+    const array = JSON.parse(recipe);
+    return array;
+   }
 }
 
 /**
@@ -35,10 +43,16 @@ function getRecipesFromStorage() {
  */
 function addRecipesToDocument(recipes) {
   // A10. TODO - Get a reference to the <main> element
+  const el = document.querySelector('main');
   // A11. TODO - Loop through each of the recipes in the passed in array,
   //            create a <recipe-card> element for each one, and populate
   //            each <recipe-card> with that recipe data using element.data = ...
   //            Append each element to <main>
+  recipes.forEach(recipe => {
+    let recCard = document.createElement('recipe-card');
+    recCard.data = recipe.data;
+    el.append(recCard);
+  });
 }
 
 /**
